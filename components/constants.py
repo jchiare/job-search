0b062fb4f731354ff0b3d6ec824eea59720ab034
Jay Chiarella <jay.gch93@gmail.com>
2023-08-16 21:34:12 +0200

    fix host

--- components/constants.py
+++ components/constants.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 DATA_ANALYSIS_BASE_ENDPOINT = (
-    "http://" + os.getenv("HOST") + ":" + os.getenv("DATA_ANALYSIS_PORT")
+    "http://" + "job-search.dataanalysis.1" + ":" + os.getenv("DATA_ANALYSIS_PORT")
 )
 WEB_BASE_ENDPOINT = "http://" + os.getenv("HOST") + ":" + os.getenv("WEB_PORT")
 
 DATA_COLLECTOR_BASE_ENDPOINT = (
-    "http://" + os.getenv("HOST") + ":" + os.getenv("DATA_COLLECTOR_PORT")
+    "http://" + "job-search.datacollector.1" + ":" + os.getenv("DATA_COLLECTOR_PORT")
 )
