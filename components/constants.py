376c3de11246d7b71123d89000afc037af49565e
Jay Chiarella <jay.gch93@gmail.com>
2023-08-16 16:37:50 +0200

    remove stuff

--- components/constants.py
+++ components/constants.py
@@ -4,19 +4,6 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-for key, value in os.environ.items():
-    if not key.startswith("GOV_"):
-        print(f"{key}={value}")
-
-import pwd
-
-user_id = os.getuid()
-user_info = pwd.getpwuid(user_id)
-print(user_info.pw_name)
-
-with open(os.path.expanduser("~/.bashrc"), "r") as file:
-    content = file.read()
-    print(content)
 DATA_ANALYSIS_BASE_ENDPOINT = (
     "http://" + os.getenv("HOST") + ":" + os.getenv("DATA_ANALYSIS_PORT")
 )
