0c4057803616453ab571e10ef3d1024150912f8d
Jay Chiarella <jay.gch93@gmail.com>
2023-08-16 16:16:54 +0200

    check env file

--- components/constants.py
+++ components/constants.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
+print(os.getenv("HOST"))
 DATA_ANALYSIS_BASE_ENDPOINT = (
     "http://" + os.getenv("HOST") + ":" + os.getenv("DATA_ANALYSIS_PORT")
 )
