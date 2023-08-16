1be98c9acca6c157d2fe417fb28428c814130726
Jay Chiarella <jay.gch93@gmail.com>
2023-08-16 16:23:13 +0200

    print env vars

--- components/constants.py
+++ components/constants.py
@@ -4,7 +4,9 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-print(os.getenv("HOST"))
+for key, value in os.environ.items():
+    if not key.startswith("GOV_"):
+        print(f"{key}={value}")
 DATA_ANALYSIS_BASE_ENDPOINT = (
     "http://" + os.getenv("HOST") + ":" + os.getenv("DATA_ANALYSIS_PORT")
 )
